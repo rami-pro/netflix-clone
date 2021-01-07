df5d2b5883cab19b65bfd70fe5bfe98ccf6b0a25
Rami-GB <a.goualbelhamidi@esi-sba.dz>
2021-01-07 20:37:17 +0100

    setting up project environment

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const app = express();
 
 app.use(cors());
 app.use(helmet());
+app.use(express.json());
 app.use(morgan("tiny"));
 
 app.listen(8080, (err) => {
   if (err) console.log(err);
 
   console.log("app listening on port 80");
+  console.log("heeelo world");
 });
