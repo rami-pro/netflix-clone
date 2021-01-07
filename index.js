79440d0c3cf37710de5638f7392ec3c49e40cec3
Rami-GB <a.goualbelhamidi@esi-sba.dz>
2021-01-07 19:15:35 +0100

    init project

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("tiny"));
+
+app.listen(8080, (err) => {
+  if (err) console.log(err);
+
+  console.log("app listening on port 80");
+});
