904c1a24889ea9ff4e87fc9ec2f8e9d8ffd7870a
Rami-GB <a.goualbelhamidi@esi-sba.dz>
2021-01-07 21:34:22 +0100

    adding error handeling

--- index.js
+++ index.js
@@ -11,7 +11,16 @@ app.use(helmet());
 app.use(express.json());
 app.use(morgan("tiny"));
 
-app.use(placesRoutes);
+app.use("/api/places", placesRoutes);
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res
+    .status(error.code || 500)
+    .send({ message: error.message || "Unknown error occured on the server" });
+});
 
 app.listen(8080, (err) => {
   if (err) console.log(err);
