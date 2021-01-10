9781d8f3a640a63eda0d2300993f2b435701fcfc
Rami-GB <a.goualbelhamidi@esi-sba.dz>
2021-01-10 04:27:30 +0100

    adding place controllers

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,19 +1,11 @@
 const { nanoid } = require("nanoid");
+const mongoose = require("mongoose");
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 const Place = require("../models/place");
+const User = require("../models/user");
 const geocode = require("../util/location");
-
-let PLACES = [
-  {
-    state: "algeria",
-    address: "Oran, Santa Cruze",
-    monument: "fort de santa cruz",
-    id: "p1",
-    creatorId: "u1",
-  },
-];
 
 const getPlaceById = async (req, res, next) => {
   let place;
@@ -30,23 +22,36 @@ const getPlaceById = async (req, res, next) => {
 };
 
 const getPlacesByUserId = async (req, res, next) => {
-  let place;
+  //let place;
+  let userWithPlaces;
   try {
-    place = await Place.find({ creator: req.params.uid });
+    userWithPlaces = await User.findById(req.params.uid).populate("places");
+    if (!userWithPlaces) return next(new HttpError("no place found", 404));
   } catch (error) {
     return next(new HttpError("internal error can't find a place", 500));
   }
 
-  if (!place) {
+  if (!userWithPlaces.places.length) {
     return next(new HttpError("no place found", 404));
   }
-  res.send(place.map((place) => place.toObject({ getters: true })));
+  res.send(
+    userWithPlaces.places.map((place) => place.toObject({ getters: true }))
+  );
 };
 
 const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return next(new HttpError("Invalid inputs data", 422));
+  }
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ _id: req.body.creator });
+    if (!existingUser)
+      return next(new HttpError("no user corresponding found.", 404));
+  } catch (error) {
+    return next(new HttpError("internal server error", 500));
   }
 
   let coordinates;
@@ -59,11 +64,16 @@ const createPlace = async (req, res, next) => {
     ...req.body,
     image: "https://via.placeholder.com/150/771796",
     location: coordinates,
-    creator: nanoid(),
+    creator: existingUser._id,
   });
 
   try {
-    await createdPlace.save();
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await createdPlace.save({ session: sess });
+    existingUser.places.push(createdPlace);
+    await existingUser.save({ session: sess });
+    await sess.commitTransaction();
   } catch (error) {
     return next(new HttpError("creating a place failed", 500));
   }
@@ -100,20 +110,28 @@ const updatePlaceById = async (req, res, next) => {
 const deletePlaceById = async (req, res, next) => {
   let place;
   try {
-    place = await Place.findOne({ _id: req.params.pid });
+    place = await Place.findOne({ _id: req.params.pid }).populate("creator");
   } catch (error) {
+    console.log(error);
     return next(new HttpError("query error", 500));
   }
 
   if (!place) {
     return next(new HttpError("no place found", 404));
   }
+
   try {
-    await place.remove();
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await place.remove({ session: sess });
+    place.creator.places.pull(place);
+    await place.creator.save({ session: sess });
+    await sess.commitTransaction();
   } catch (error) {
     console.log(error);
     return next(new HttpError("deleting a place failed", 500));
   }
+
   res.send();
 };
 
